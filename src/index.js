e1d1d18d70ee828bb9d29618b0fc5a954e0fb3a1
perpera <131678085+perpera@users.noreply.github.com>
2023-10-07 17:46:20 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const apiKey = '39896651-9a027bdc3823726f67ea1473a';
